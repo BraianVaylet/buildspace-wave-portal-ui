1187557f8dd6b801b913d7cad9d67db991a31992
Braian Vaylet <braianvaylet@gmail.com>
2022-01-12 20:15:33 -0300

    add chakraui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component }) {
+    return (
+    <ChakraProvider>
+      <Component />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
